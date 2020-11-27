b7a2f55d7d966787b34fa9714df55d8daec15e42
Temitayo Ogunsusi <ogunsusitemitayo99@gmail.com>
2020-11-27 23:12:43 +0100

    Implemented User login

--- controllers/authController.js
+++ controllers/authController.js
@@ -56,3 +56,26 @@ exports.signup = catchAsync(async (req, res, next) => {
 
 	createSendToken(newUser, 201, req, res);
 });
+
+exports.login = catchAsync(async (req, res, next) => {
+	const { email, password } = req.body;
+
+	// 1) Check if email and password exists
+	if (!email || !password) {
+		return next(new AppError('Please provide email and password!', 400));
+	}
+	// 2) Check if user exists && password is correct
+	const user = await User.findOne({ email }).select('+password');
+
+	if (!user || !(await user.correctPassword(password, user.password))) {
+		return next(new AppError('Incorrect email or password', 401));
+	}
+
+	// Update last login field
+	await User.findByIdAndUpdate(user._id, {
+		lastLoggedIn: Date.now(),
+	});
+
+	// 3) If everything is okay, send token to client
+	createSendToken(user, 200, req, res);
+});
